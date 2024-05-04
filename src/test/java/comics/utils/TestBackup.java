f4585d860531da2a5ea8f71c54c0d40e0ba9974a
Jorge Bau Gracia <jbaugracia@gmail.com>
2024-05-04 16:42:53 +0200

    Improved tests - particular cases of backup

--- src/test/java/comics/utils/TestBackup.java
+++ src/test/java/comics/utils/TestBackup.java
@@ -1,13 +1,18 @@
 package comics.utils;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import shell.OSDetection;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 
+import static comics.utils.TestUtils.copyResource;
+import static comics.utils.TestUtils.runTest;
+import static java.util.List.of;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -16,23 +21,54 @@ public class TestBackup {
     @Test
     public void testBackup() {
         try {
-            TestUtils.runTest((File directory) -> {
-                // Try to move something to the backup directory
-                var targetFile = new File(directory, "test.cbz");
-                TestUtils.copyResource("/compressed/test.cbz", targetFile);
-                assertTrue(targetFile.exists());
-                var originalHash = TestUtils.md5(targetFile);
-                new BackupService(directory).backupFile(targetFile);
-                assertFalse(targetFile.exists());
-                var today = TestUtils.today();
-                var backupFile = new File(directory, String.format(".comicutils/%s/test.cbz", today));
-                assertTrue(backupFile.exists());
-                assertFalse(backupFile.isDirectory());
-                assertFalse(Files.isSymbolicLink(backupFile.toPath()));
-                Assertions.assertEquals(originalHash, TestUtils.md5(backupFile));
+            runTest((File directory) -> {
+                of("test.cbz", "test.cbr").forEach(file -> {
+                    // Try to move something to the backup directory
+                    var targetFile = new File(directory, file);
+                    copyResource("/compressed/" + file, targetFile);
+                    assertTrue(targetFile.exists());
+                    var originalHashCbz = TestUtils.md5(targetFile);
+                    try { new BackupService(directory).backupFile(targetFile); } catch (IOException ioe) { fail(ioe); }
+                    assertFalse(targetFile.exists());
+                    var today = TestUtils.today();
+                    var backupFile = new File(directory, String.format(".comicutils/%s/" + file, today));
+                    assertTrue(backupFile.exists());
+                    assertFalse(backupFile.isDirectory());
+                    assertFalse(Files.isSymbolicLink(backupFile.toPath()));
+                    assertEquals(originalHashCbz, TestUtils.md5(backupFile));
+                });
             });
         } catch (Exception e) {
             fail(e);
         }
+    }
+
+    @Test
+    public void cannotCreateBackupDir() {
+        runTest((File directory) -> {
+            File bogus = new File(directory, ".comicutils");
+            bogus.createNewFile();
+            File realFile = new File(directory, "something");
+            realFile.createNewFile();
+            assertThrowsExactly(IOException.class, () -> new BackupService(directory).backupFile(realFile));
+        });
+    }
+
+    @Test
+    public void testErrorCases() {
+        runTest((File directory) -> {
+            assertThrowsExactly(AssertionError.class, () -> new BackupService(directory).backupFile(null));
+            assertThrowsExactly(AssertionError.class, () -> new BackupService(directory).backupFile(directory));
+            assertThrowsExactly(AssertionError.class, () -> new BackupService(directory).backupFile(new File(directory, "does not exist")));
+            if (!OSDetection.isWindows()) {
+                // This requires admin permissions in windows T_T
+                File realFile = new File(directory, "realFile");
+                realFile.createNewFile();
+                Files.write(realFile.toPath(), "trololo".getBytes("UTF-8"));
+                File symlink = new File(directory, "symlink");
+                Files.createSymbolicLink(symlink.toPath(), realFile.toPath());
+                assertThrowsExactly(AssertionError.class, () -> new BackupService(directory).backupFile(symlink));
+            }
+        });
     }
 }
